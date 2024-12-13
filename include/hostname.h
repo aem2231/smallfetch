1e9f14cbf8a3597b51144c60871bffaf470aff34
aem2231 <mariojetullaji@gmail.com>
2024-12-13 23:18:19 +0000

    added hostname.c to retrieve hostname

---
+++ include/hostname.h
@@ -0,0 +1,6 @@
+#ifndef HOSTNAME_H_
+#define HOSTNAME_H_
+
+int get_hostname(char*, size_t);
+
+#endif // HOSTNAME_H_
--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "cpu.h"
+#include "hostname.h"
 
 int main(){
     int ram, ram_usage, packages;
     char hostname[256], kernel_version[256], cpu_name[256];
 
     get_cpu_model(cpu_name, sizeof(cpu_name));
+    get_hostname(hostname, sizeof(hostname));
     printf("%s", cpu_name);
+    printf("%s", hostname);
 }
 
 
