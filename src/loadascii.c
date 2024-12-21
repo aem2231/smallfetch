15f3252480ac6940f4bfae0484806e495341cd08
aem2231 <mariojetullaji@gmail.com>
2024-12-21 13:02:29 +0000

    improved memory allocation

--- src/loadascii.c
+++ src/loadascii.c
@@ -13,42 +13,58 @@ int load_ascii_art(char*** ascii_art, int* longest_line, int* num_lines, char* b
         printf("Error opening ASCII art file.\n");
         return -1;
     }
+    int capacity = 8;
+    *ascii_art = (char **)malloc(capacity * sizeof(char *));
+    if (*ascii_art == NULL) {
+        fclose(fp);
+        printf("Error allocating initial memory.\n");
+        return -1;
+    }
 
-    *ascii_art = NULL;
+    *num_lines = 0;
+    *longest_line = 0;
+
+    // Read lines from the file
     while (fgets(buffer, buffer_size, fp) != NULL) {
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        *ascii_art = (char **)realloc(*ascii_art, (*num_lines + 1) * sizeof(char *));
-        if (*ascii_art == NULL) {
-            fclose(fp);
-            for (int i = 0; i < *num_lines; i++) {
-                free((*ascii_art)[i]);
+        if (*num_lines >= capacity) {
+            capacity *= 2;
+            char **temp = (char **)realloc(*ascii_art, capacity * sizeof(char *));
+            if (temp == NULL) {
+                for (int i = 0; i < *num_lines; i++) {
+                    free((*ascii_art)[i]);
+                }
+                free(*ascii_art);
+                fclose(fp);
+                *ascii_art = NULL;
+                printf("Error reallocating memory.\n");
+                return -1;
             }
-            free(*ascii_art);
-            *ascii_art = NULL;
-            printf("Error reallocating memory.\n");
-            return -1;
+            *ascii_art = temp;
         }
 
-        (*ascii_art)[*num_lines] = (char *)malloc(strlen(buffer) + 1);
+        size_t line_length = strlen(buffer) + 1;
+        (*ascii_art)[*num_lines] = (char *)malloc(line_length);
         if ((*ascii_art)[*num_lines] == NULL) {
-            fclose(fp);
+            // Free previously allocated lines on malloc failure
             for (int i = 0; i < *num_lines; i++) {
                 free((*ascii_art)[i]);
             }
             free(*ascii_art);
+            fclose(fp);
             *ascii_art = NULL;
-            printf("Error allocating memory.\n");
+            printf("Error allocating memory for a line.\n");
             return -1;
-        }
-
-        if (strlen(buffer) > *longest_line) {
-            *longest_line = strlen(buffer);
         }
 
         strcpy((*ascii_art)[*num_lines], buffer);
+        if (line_length - 1 > *longest_line) {
+            *longest_line = line_length - 1;
+        }
         (*num_lines)++;
     }
+
     fclose(fp);
     return *num_lines;
 }
